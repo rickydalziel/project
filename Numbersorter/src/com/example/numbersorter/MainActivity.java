cdbbe6c049294ed97e6b65772c76c7f1c8bf2065
rickydalziel <rickyd493@hotmail.com>
2013-02-01 10:35:48 +0000

    Added fucntionality to exit and about buttons, also changed functionality of continue game button to be invisible when there is no saved game.

--- Numbersorter/src/com/example/numbersorter/MainActivity.java
+++ Numbersorter/src/com/example/numbersorter/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.numbersorter;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,11 +13,41 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loadbutton = (Button)findViewById(R.id.continue_game);
+        if(!hasSavedGame()){
+        	
+        	
+        	loadbutton.setVisibility(View.INVISIBLE);
+        }
+        else{
+        	
+        	loadbutton.setVisibility(View.VISIBLE);
+        	
+        }
     }
 
-    @Override
+    private boolean hasSavedGame() {
+    	
+		return false;
+		
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void exitPressed(View v){
+    	
+    	finish();
+    	System.exit(0);
+    }
+    
+    public void aboutPressed(View v){
+    	
+    	Intent i = new Intent(this, AboutActivity.class);
+    	startActivity(i);
+    	
     }
 }
